f3b3b45cd58e585831b4917a4c1f6c7b216eb229
jmcarth4@gmail.com <jmcarth4@gmail.com>
2023-10-05 17:51:24 -0600

    Add project files.

---
+++ Class_9.27/Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DisplayButton = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.QuitButton = New System.Windows.Forms.Button()
+        Me.SaveButton = New System.Windows.Forms.Button()
+        Me.ReadFileButton = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'DisplayButton
+        '
+        Me.DisplayButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(40, Byte), Integer), CType(CType(40, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.DisplayButton.Font = New System.Drawing.Font("Verdana", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DisplayButton.Location = New System.Drawing.Point(34, 341)
+        Me.DisplayButton.Margin = New System.Windows.Forms.Padding(7, 6, 7, 6)
+        Me.DisplayButton.Name = "DisplayButton"
+        Me.DisplayButton.Size = New System.Drawing.Size(204, 94)
+        Me.DisplayButton.TabIndex = 2
+        Me.DisplayButton.Text = "&Display"
+        Me.DisplayButton.UseVisualStyleBackColor = False
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Location = New System.Drawing.Point(304, 3)
+        Me.ListBox1.Margin = New System.Windows.Forms.Padding(7, 6, 7, 6)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(192, 444)
+        Me.ListBox1.TabIndex = 3
+        '
+        'QuitButton
+        '
+        Me.QuitButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(40, Byte), Integer), CType(CType(40, Byte), Integer), CType(CType(40, Byte), Integer))
+        Me.QuitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.QuitButton.Font = New System.Drawing.Font("Verdana", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.QuitButton.Location = New System.Drawing.Point(664, 357)
+        Me.QuitButton.Margin = New System.Windows.Forms.Padding(7, 6, 7, 6)
+        Me.QuitButton.Name = "QuitButton"
+        Me.QuitButton.Size = New System.Drawing.Size(120, 78)
+        Me.QuitButton.TabIndex = 4
+        Me.QuitButton.Text = "Quit"
+        Me.QuitButton.UseVisualStyleBackColor = False
+        '
+        'SaveButton
+        '
+        Me.SaveButton.Location = New System.Drawing.Point(517, 215)
+        Me.SaveButton.Name = "SaveButton"
+        Me.SaveButton.Size = New System.Drawing.Size(123, 94)
+        Me.SaveButton.TabIndex = 5
+        Me.SaveButton.Text = "Save"
+        Me.SaveButton.UseVisualStyleBackColor = True
+        '
+        'ReadFileButton
+        '
+        Me.ReadFileButton.Location = New System.Drawing.Point(517, 341)
+        Me.ReadFileButton.Name = "ReadFileButton"
+        Me.ReadFileButton.Size = New System.Drawing.Size(123, 94)
+        Me.ReadFileButton.TabIndex = 6
+        Me.ReadFileButton.Text = "Read"
+        Me.ReadFileButton.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(59, 42)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 20)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Data"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ReadFileButton)
+        Me.Controls.Add(Me.SaveButton)
+        Me.Controls.Add(Me.QuitButton)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.DisplayButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DisplayButton As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents QuitButton As Button
+    Friend WithEvents SaveButton As Button
+    Friend WithEvents ReadFileButton As Button
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Class_9.27/Form1.vb
@@ -0,0 +1,119 @@
+﻿
+'Jessica McArthur
+'RCET 3371
+'HW 3-1
+'26 Sept 2023
+
+Option Strict On
+Option Explicit On
+
+Public Class Form1
+    Dim numArray(10) As Integer
+
+    'Varialbes to write file
+    Private Const Prompt As String = "File Not Found"
+    Dim dateTemp As String
+    Dim fileName As String
+    Dim drivePath As String
+
+
+    'not needed...from past example
+    'Displays a random integer from -10 to 10 is displayed in a listbox.
+    Private Sub DisplayButton_Click(sender As Object, e As EventArgs) Handles DisplayButton.Click
+
+            ListBox1.Items.Clear()                          'Clears listbox
+
+            numArray(0) = RandomNumber()                    'Set content of array to randomize number
+            numArray(1) = RandomNumber()
+            numArray(2) = RandomNumber()
+            numArray(3) = RandomNumber()
+            numArray(4) = RandomNumber()
+            numArray(5) = RandomNumber()
+            numArray(6) = RandomNumber()
+            numArray(7) = RandomNumber()
+            numArray(8) = RandomNumber()
+            numArray(9) = RandomNumber()
+            ListBox1.Items.Add(Str(numArray(0)))              'Displays output in listbox
+            ListBox1.Items.Add(Str(numArray(1)))
+            ListBox1.Items.Add(Str(numArray(2)))
+            ListBox1.Items.Add(Str(numArray(3)))
+            ListBox1.Items.Add(Str(numArray(4)))
+            ListBox1.Items.Add(Str(numArray(5)))
+            ListBox1.Items.Add(Str(numArray(6)))
+            ListBox1.Items.Add(Str(numArray(7)))
+            ListBox1.Items.Add(Str(numArray(8)))
+            ListBox1.Items.Add(Str(numArray(9)))
+        End Sub
+
+    'Function to generate random integer from -10 to 10.
+    Function RandomNumber() As Integer
+            Return CInt((Rnd() * 21) - 10)
+        End Function
+
+    'Exits the program when button is pressed.
+    Private Sub QuitButton_Click(sender As Object, e As EventArgs) Handles QuitButton.Click
+            Me.Close()
+        End Sub
+
+    'How to write file
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dateTemp = DateString  'Sets current date from computer
+        'Set file name to be created : "CLASS and current date value
+        'works
+        'fileName = "C:\Users\jekam\Documents\CLASS_" & dateTemp & ".txt"
+
+
+        'Saves file to debug of current project
+        'works
+        fileName = CurDir() & "\" & dateTemp & ".txt"
+        ' not work
+        'fileName = drivePath & "\" & dateTemp & ".txt"
+
+
+
+    End Sub
+
+    Private Sub SaveButton_Click(sender As Object, e As EventArgs) Handles SaveButton.Click
+        Try
+            FileOpen(1, fileName, OpenMode.Append) 'Open file for Append
+        Catch ex As Exception
+            'Doing nothing
+        End Try
+
+        'Line of data must be same lenght of items or will not read file....crashes program
+        'both options work
+        'WriteLine(1, "      ", RandomNumber)
+        WriteLine(1, Label2.Text, RandomNumber)
+
+        FileClose(1)
+    End Sub
+
+    Private Sub ReadFileButton_Click(sender As Object, e As EventArgs) Handles ReadFileButton.Click
+        Dim Record(1000, 3) As String
+        Dim ptr As Int32
+        Dim lineCount As Integer
+        lineCount = 1
+        ListBox1.Items.Clear()
+
+        Try
+            FileOpen(1, fileName, OpenMode.Input) 'Open file for read
+
+        Catch ex As Exception
+            MsgBox(Prompt)
+        End Try
+
+        Do While Not EOF(1) 'Read until end of file
+            Input(1, Record(ptr, 0))
+            Input(1, Record(ptr, 1))
+            ListBox1.Items.Add(Str(lineCount) & vbTab & Record(ptr, 0) & vbTab & Record(ptr, 1))
+            If ptr >= UBound(Record, 1) Then Exit Do
+            ptr += 1
+            lineCount += 1
+        Loop
+        FileClose(1)
+    End Sub
+End Class
+
+
